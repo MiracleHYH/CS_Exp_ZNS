d8f77f7f2dda0a2b2e7f22fb58de2a223a7394a5
MiracleHYH <miracle20001110@gmail.com>
2022-11-08 10:33:21 +0800

    update

---
+++ work/task2/hello_miracle/hello_miracle.c
@@ -0,0 +1,12 @@
+#include "spdk/stdinc.h"
+#include "spdk/nvme.h"
+#include "spdk/vmd.h"
+#include "spdk/nvme_zns.h"
+#include "spdk/env.h"
+#include "spdk/string.h"
+#include "spdk/log.h"
+
+int main(int argc, char **argv)
+{
+
+}
