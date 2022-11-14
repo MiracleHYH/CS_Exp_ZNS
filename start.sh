722d3a227b3856645d7a15f2db80305661f75b0d
MiracleHYH <miracle20001110@gmail.com>
2022-11-14 15:21:47 +0800

    fix start.sh

--- start.sh
+++ start.sh
@@ -24,15 +24,29 @@ else
     exit
 fi
 
-qemu-system-x86_64 --enable-kvm \
--nographic \
--name cs-exp-zns \
--m 8G \
--cpu host -smp 4 \
--hda ./env/ubuntu.qcow2 \
--net user,hostfwd=tcp:127.0.0.1:7777-:22,hostfwd=tcp:127.0.0.1:2222-:2000 -net nic \
--drive file=./env/$ZNSSSD_NAME,id=mynvme,format=$ZNSSSD_FORMAT,if=none \
--device nvme,serial=baz,id=nvme2 \
--device nvme-ns,id=ns2,drive=mynvme,nsid=2,logical_block_size=4096,physical_block_size=4096,zoned=true,zoned.zone_size=131072,zoned.zone_capacity=131072,zoned.max_open=0,zoned.max_active=0,bus=nvme2 \
--fsdev local,id=fsdev0,path=./work/,security_model=none \
--device virtio-9p-pci,id=fs0,fsdev=fsdev0,mount_tag=hostshare
+if [ $ZNSSSD_NAME == znsssd.qcow2 ]; then
+    qemu-system-x86_64 --enable-kvm \
+    -nographic \
+    -name cs-exp-zns \
+    -m 8G \
+    -cpu host -smp 4 \
+    -hda ./env/ubuntu.qcow2 \
+    -net user,hostfwd=tcp:127.0.0.1:7777-:22,hostfwd=tcp:127.0.0.1:2222-:2000 -net nic \
+    -drive file=./env/$ZNSSSD_NAME,id=mynvme,format=$ZNSSSD_FORMAT,if=none \
+    -device nvme,serial=baz,id=nvme2 \
+    -device nvme-ns,id=ns2,drive=mynvme,nsid=2,logical_block_size=4096,physical_block_size=4096,zoned=true,zoned.zone_size=131072,zoned.zone_capacity=131072,zoned.max_open=0,zoned.max_active=0,bus=nvme2 \
+    -fsdev local,id=fsdev0,path=./work/,security_model=none \
+    -device virtio-9p-pci,id=fs0,fsdev=fsdev0,mount_tag=hostshare
+else
+    qemu-system-x86_64 --enable-kvm \
+    -nographic \
+    -name cs-exp-zns \
+    -m 8G \
+    -cpu host -smp 4 \
+    -hda ./env/ubuntu.qcow2 \
+    -net user,hostfwd=tcp:127.0.0.1:7777-:22,hostfwd=tcp:127.0.0.1:2222-:2000 -net nic \
+    -drive file=./env/$ZNSSSD_NAME,id=mynvme,format=$ZNSSSD_FORMAT,if=none \
+    -device nvme,serial=baz,drive=mynvme \
+    -fsdev local,id=fsdev0,path=./work/,security_model=none \
+    -device virtio-9p-pci,id=fs0,fsdev=fsdev0,mount_tag=hostshare
+fi
