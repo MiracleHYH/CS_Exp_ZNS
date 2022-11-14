59c69f09ffac647088992ae6a0d06525c53a0db8
MiracleHYH <miracle20001110@gmail.com>
2022-11-14 16:31:01 +0800

    init miracle_bdev

---
+++ work/task3/miracle_bdev/miracle_bdev.c
@@ -0,0 +1,16 @@
+#include "spdk/stdinc.h"
+#include "spdk/thread.h"
+#include "spdk/bdev.h"
+#include "spdk/env.h"
+#include "spdk/event.h"
+#include "spdk/log.h"
+#include "spdk/string.h"
+#include "spdk/bdev_zone.h"
+
+
+int main(int argc, char const *argv[])
+{
+    /* code */
+    printf("miracle_bedv\n");
+    return 0;
+}
